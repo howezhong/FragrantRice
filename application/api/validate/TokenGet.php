2f2f06d1d226f164ea4cdb1eca1b0006ce58579a
howezhong <zanely163@163.com>
2018-05-23 17:34:43 +0800

    add TokenGet controller

---
+++ application/api/validate/TokenGet.php
@@ -0,0 +1,13 @@
+<?php
+namespace app\api\validate;
+
+class TokenGet extends BaseValidate
+{
+	protected $rule = [
+        'code' => 'require|isNotEmpty'
+    ];
+    
+    protected $message=[
+        'code' => 'code参数必须'
+    ];
+}
