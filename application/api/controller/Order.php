44af6a909e4b2c7c81163fcac4456dda9fce5c0d
howezhong <zanely163@163.com>
2018-05-31 15:26:23 +0800

    perfect Order

--- application/api/controller/Order.php
+++ application/api/controller/Order.php
@@ -2,12 +2,21 @@
 
 namespace app\api\controller;
 
+use app\api\validate\OrderPlace;
+use app\api\service\Token as TokenService;
+use app\api\service\Order as OrderService;
+
 class Order
 {
     /**
      * 下订单
      */
     public function placeOrder() {
-
+        (new OrderPlace())->goCheck();
+        $products = input('post.products/a');
+        $uid = TokenService::getCurrentUid();
+        $order = new OrderService();
+        $status = $order->place($uid, $products);
+        return $status;
     }
 }
