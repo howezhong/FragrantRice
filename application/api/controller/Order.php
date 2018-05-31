4ebb19e94ed9561503a17f70d4a5ac1d3c8913cd
howezhong <zanely163@163.com>
2018-05-31 17:45:05 +0800

    perfect Order

--- application/api/controller/Order.php
+++ application/api/controller/Order.php
@@ -3,8 +3,11 @@
 namespace app\api\controller;
 
 use app\api\validate\OrderPlace;
+use app\api\validate\IDMustBePositiveInt;
 use app\api\service\Token as TokenService;
 use app\api\service\Order as OrderService;
+use app\api\model\Order as OrderModel;
+use app\lib\exception\OrderException;
 
 class Order
 {
@@ -17,6 +20,21 @@ class Order
         $uid = TokenService::getCurrentUid();
         $order = new OrderService();
         $status = $order->place($uid, $products);
-        return $status;
+        return json($status);
+    }
+
+    /**
+     * 获取订单详情
+     * @param $id
+     * @return json
+     */
+    public function getDetail($id)
+    {
+        (new IDMustBePositiveInt())->goCheck();
+        $orderDetail = OrderModel::get($id);
+        if (!$orderDetail) {
+            throw new OrderException();
+        }
+        return json($orderDetail->hidden(['prepay_id']));
     }
 }
--- application/api/model/Order.php
+++ application/api/model/Order.php
@@ -2,7 +2,9 @@
 
 namespace app\api\model;
 
-class Order
+class Order extends BaseModel
 {
-
+    protected $hidden = ['user_id', 'delete_time', 'update_time'];
+    protected $autoWriteTimestamp = true;
+    
 }
